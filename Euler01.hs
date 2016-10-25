158b5566948dc922a1dfc8ba7f826aaec091e26e
Pavitra Saiprasad <saiprasad.pavitra@gmail.com>
2016-10-25 18:03:33 +0100

    removed a redundant function

--- Euler01.hs
+++ Euler01.hs
@@ -4,12 +4,7 @@ import Data.List
 calculateSumOfAllMultiplesOf3Or5 :: Int -> Int -> Int -> Int
 calculateSumOfAllMultiplesOf3Or5 start end stepSize = foldl1 (+) z
   where
-  y = createAListOfNumbers start end stepSize
-  z = filter multipleOf3Or5 y
-  
-  
-createAListOfNumbers :: Int -> Int -> Int -> [Int]
-createAListOfNumbers start end stepSize = [start,start+stepSize..end]
+  z = filter multipleOf3Or5 [start,start+stepSize..end]
 
 multipleOf3Or5 :: Int -> Bool
 multipleOf3Or5 input = mod input 3 == 0 || mod input 5 == 0
